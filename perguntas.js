1b0db946b32a9b8b33d986ce9a34fae03d4c8f49
Jefterddf28 <00001124407054sp@al.educacao.sp.gov.br>
2024-11-07 11:44:41 -0300

    Adicionando arquivos HTML

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Facil',
+    'Quantos personagens principal existe no GTA 5?',
+    'Três (Franklin, Michael e Trevor)'
+)
+
+criaCartao(
+    'Médio',
+    'Quando lançou GTA 5?',
+    '17 de Setembro de 2013'
+)
+
+criaCartao(
+    'Dificil',
+    'Onde se passa a primeira missão do GTA 5?',
+    'Ludendorff - North Yankton'
+)
+
+criaCartao(
+    'Facil',
+    'Qual nome do cachorro do Franklin?',
+    'Chop'
+)
